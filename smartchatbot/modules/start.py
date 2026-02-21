a9dbb48701c5f4417787c2b80338f33699f47dab
rajayns42-design <rajayns42@gmail.com>
2026-02-21 08:03:04 +0530

    Update start.py

--- smartchatbot/modules/start.py
+++ smartchatbot/modules/start.py
@@ -61,6 +61,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "๏ 𝗛𝗢𝗪 𝗧𝗢 𝗨𝗦𝗘 𝗠𝗘 ?\n"
         "➜ Aᴅᴅ Mᴇ Bᴀʙʏ ʏᴏᴜʀ Gʀᴏᴜᴩ\n"
         "➜ Uꜱᴇ /Chatbot Oɴ ᴛᴏ Eɴᴀʙʟᴇ\n\n"
+        "➜ Uꜱᴇ /Chatbot σꜰꜰ ᴛᴏ Dɪꜱᴀʙʟᴇ\n\n"
         "➜ Cʟɪᴄᴋ Tʜᴇ Hᴇʟᴩ Bᴜᴛᴛᴏɴ Fᴏʀ Mᴏʀᴇ 🫶\n"
         f"</blockquote>"
     )
